cee629d64b9a2a8aba978086feea380ab8a4b357
P3D\arthur.balazun <arthur.balazun@piktura.fr>
2026-02-02 17:50:08 +0100

    [GP] Map Generation

--- Assets/2 - Scripts/PlayerController.cs
+++ Assets/2 - Scripts/PlayerController.cs
@@ -7,10 +7,19 @@ public class PlayerController : MonoBehaviour
     [Range(5f, 10f)]
     [SerializeField] float _speedPlayerMove;
 
+    int _boxPositionX;
+    int _boxPositionZ;
+    public int BoxPositionX => _boxPositionX;
+    public int BoxPositionZ => _boxPositionZ;
 
     public void MoveCharacter(Vector3 targetPos)
     {
         transform.position = Vector3.Lerp(transform.position, new Vector3(targetPos.x, transform.position.y, targetPos.z), _speedPlayerMove * Time.deltaTime);
+    }
+
+    public void SetBoxPosition()
+    {
+
     }
 
 }
