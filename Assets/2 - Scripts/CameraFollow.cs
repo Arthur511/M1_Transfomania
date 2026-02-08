05d8eb17432800aca99aa862d0c1431c674953bd
Arthur511 <92777780+Arthur511@users.noreply.github.com>
2026-02-08 18:00:55 +0100

    [GP - Camera] Camera system with offset

--- Assets/2 - Scripts/CameraFollow.cs
+++ Assets/2 - Scripts/CameraFollow.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 
-    [Range(6f, 8f)]
+    [Range(1f, 3f)]
     [SerializeField] float _speedCamera;
     [SerializeField] Vector3 _offset;
+    public Vector3 Offset => _offset;
+    [SerializeField] Transform _levelCenter;
+    public void SetCurrentOffset(PlayerController player)
+    {
+        Vector3 distance = _levelCenter.position - player.transform.position;
+        _offset.x = distance.x/2;
+        _offset.z = distance.z/2  - 15;
+    }
 
     public void CameraMovement(Vector3 targetPos)
     {
--- Assets/2 - Scripts/MainGame.cs
+++ Assets/2 - Scripts/MainGame.cs
@@ -13,13 +13,14 @@ public class MainGame : MonoBehaviour
     //[SerializeField] GameObject _player;
     
     bool _isPlayerMoving = false;
+    bool _isCameraMoving = false;
     Vector3 _targetPosition;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        _cameraFollow.SetCurrentOffset(_playerController);
     }
 
     // Update is called once per frame
@@ -33,11 +34,17 @@ public class MainGame : MonoBehaviour
         if (_isPlayerMoving)
         {
             _playerController.MoveCharacter(_targetPosition);
-            _cameraFollow.CameraMovement(_targetPosition);
             if ((transform.position - _targetPosition).sqrMagnitude < 0.01f)
                 _isPlayerMoving = false;
         }
 
+        if (_isCameraMoving)
+        {
+            _cameraFollow.SetCurrentOffset(_playerController);
+            _cameraFollow.CameraMovement(_targetPosition);
+            if ((_cameraFollow.transform.position - _targetPosition + _cameraFollow.Offset).sqrMagnitude < 0.01f)
+                _isCameraMoving = false;
+        }
     }
 
     private void OnClick()
@@ -51,10 +58,10 @@ public class MainGame : MonoBehaviour
                 if (1 << hit.collider.gameObject.layer == _boxLayer.value)
                 {
                     _isPlayerMoving = true;
+                    _isCameraMoving = true;
                     _targetPosition = hit.transform.position;
                 }
             }
-
         }
     }
 }
