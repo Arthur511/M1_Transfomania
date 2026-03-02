06093f1f8d997b190be1432b044883e8d58a509f
Photographesque <144922880+Photographesque@users.noreply.github.com>
2026-03-02 16:01:19 +0100

    [GD]/[ND]
    - cinematic onboarding

--- Assets/2 - Scripts/GD/CameraSwap.cs
+++ Assets/2 - Scripts/GD/CameraSwap.cs
@@ -1,5 +1,6 @@
 using System.Collections;
-using Unity.Hierarchy;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraSwap : MonoBehaviour
@@ -8,12 +9,46 @@ public class CameraSwap : MonoBehaviour
     public GameObject MainCamera;
     public GameObject Cinematicbrain;
     public float time_before_swap_again;
-    
+
+    public float time_before_swap_material;
+    public GameObject thumbnail;
+    public Material thumbnailswapped;
+
+    public string[] introtxt;
+    public TMP_Text asset;
+    public float time_before_next_txt;
+    int count = 0;
+
+
     void Start()
     {
         MainCamera.SetActive(false);
         Cinematicbrain.SetActive(true);
+        StartCoroutine(WaitAndSwapMaterial());
         StartCoroutine(WaitAndSwapAgain());
+        asset.GetComponent<TMP_Text>().text = introtxt[0];
+        StartCoroutine(WaitAndSwapTxt());
+
+    }
+
+    IEnumerator WaitAndSwapTxt()
+    {
+
+        for (int i = 0; i < introtxt.Length; i++)
+        {
+            yield return new WaitForSeconds(time_before_next_txt);
+            asset.GetComponent<TMP_Text>().text = introtxt[count];
+            count++;
+        }
+        yield return new WaitForSeconds(time_before_next_txt+1.592f);
+        asset.enabled = false;       
+       
+    }
+
+    IEnumerator WaitAndSwapMaterial()
+    {
+        yield return new WaitForSeconds(time_before_swap_material);
+        thumbnail.GetComponent<MeshRenderer>().material = thumbnailswapped;
     }
 
     IEnumerator WaitAndSwapAgain()
@@ -21,6 +56,7 @@ public class CameraSwap : MonoBehaviour
         yield return new WaitForSeconds(time_before_swap_again);
         Cinematicbrain.SetActive(false);
         MainCamera.SetActive(true);
-        Debug.Log("worked");
     }
+
+
 }
